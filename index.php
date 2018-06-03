f38e47cec2cf38f46cd155b76f4527d3db4b77b3
Zhanuzak <kudaibergenov.zhanuzak@gmail.com>
2018-06-03 06:54:41 +0600

    make it better

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Шпор - ҰБТ 2018, жаңа басылым</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="device-mockups/device-mockups.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/new-age.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/uikit.min.css">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">ШпорKZ</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="https://api.whatsapp.com/send?phone=87777777777"><span uk-icon="whatsapp"></span> Whatsapp</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="tel:87757777777"><span uk-icon="receiver"></span> 8 (777) 777 77 77</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-outline-success nav-link js-scroll-trigger color-white" href="#" uk-toggle="target: #modal-close-outside">Тапсырыс беру</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="container h-100">
+        <div class="row h-100">
+          <div class="col-lg-10 my-auto mx-auto">
+            <div class="header-content mx-auto text-center">
+              <h1 class="mb-5 text-white">ҰБТ-2018 арналған көмекші құралдар</h1>
+              <p class="mb-5 text-white">тапсырыс бойынша Қазақстанның барлық өңіріне жеткіземіз!</p>
+              <a href="https://api.whatsapp.com/send?phone=87777777777" class="btn btn-outline btn-xl js-scroll-trigger"><span uk-icon="whatsapp"></span> Тапсырыс беріңіз</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="tovary">
+      <div class="container">
+        <div class="section-heading text-center mb-5">
+          <h2 class="section-heading text-center mb-5">Біз ұсынатын қосымша құралдар</h2>
+        </div>  
+        <div class="row">
+          <div class="col-md-6">
+             <ul class="col-md-4 uk-list mx-auto list">
+              <li>Математика</li>
+              <li>Қазақстан тарихы</li>
+              <li>Қазақ тілі</li>
+              <li>Орыс тілі</li>
+              <li>Физика</li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+             <ul class="col-md-4 uk-list mx-auto list">
+              <li>Химия</li>
+              <li>Биология</li>
+              <li>Дүние жүзілік тарих</li>
+              <li>Қоғам</li>
+              <li>Тағы басқалары</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-12 mt-5">
+          <div class="col-md-4 mx-auto text-center">
+            <a href="https://api.whatsapp.com/send?phone=87777777777" class="btn btn-outline btn-xl mx-auto w-100"><span uk-icon="whatsapp"></span> Тапсырыс беріңіз</a>
+          </div> 
+        </div>
+      </div>
+    </section>
+
+    <section class="download bg-green text-center" id="download">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+            <h3 class="section-heading mb-5 text-white">Сұрақтарыңыз болса өз нөмеріңізді қалдырыңыз, біз сізге қоңырау шаламыз</h3>
+            <form>
+              <div class="row">
+                <div class="col-md-4 mb-3">
+                  <input type="text" class="uk-input uk-form-success" placeholder="Аты-жөні">
+                </div>
+                <div class="col-md-4 mb-3">
+                  <input type="text" class="uk-input uk-form-success" placeholder="Телефон">
+                </div>
+                <div class="col-md-4 mb-3">
+                  <input type="submit" class="form-control btn btn-outline text-white" value="Жіберу">
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="features" id="features">
+      <div class="container">
+        <div class="section-heading text-center">
+          <h2>Біз ұсынып отырған қосымша құралдардың бағасы төмендегіше</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-12 my-auto">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4 mb-3">
+                  <div class="uk-card uk-card-default uk-card-body">
+                    <h3 class="uk-card-title">1 Косымша құрал</h3>
+                    <p>1800 тг-ден</p>
+                  </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                  <div class="uk-card uk-card-default uk-card-body">
+                    <h3 class="uk-card-title">3 Қосымша құрал</h3>
+                    <p>1500 тг-ден</p>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="uk-card uk-card-default uk-card-body">
+                    <h3 class="uk-card-title">5 Қосымша құрал</h3>
+                    <p>1200 тг-ден</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12 mt-5">
+          <div class="col-md-4 mx-auto text-center">
+            <a href="#" class="btn btn-outline btn-xl mx-auto w-100" uk-toggle="target: #modal-close-outside"><span uk-icon="whatsapp"></span> Тапсырыс беріңіз</a>
+          </div> 
+        </div>
+      </div>
+    </section>
+
+    <section class="otz">
+      <div class="container">
+        <div class="section-heading text-center">
+          <h2>Біз жайлы пікірлері</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-12 mx-auto mt-5">
+            <div uk-slider>
+
+              <div class="uk-position-relative uk-visible-toggle uk-light">
+
+                  <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+                      <li>
+                          <img src="img/1.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">1</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/2.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">2</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/3.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">3</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/4.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">4</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/5.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">5</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/6.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">6</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/7.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">7</h1></div>
+                      </li>
+                      <li>
+                          <img src="img/8.jpeg" alt="">
+                          <div class="uk-position-center uk-panel"><h1 style="color: #40DF6D">8</h1></div>
+                      </li>
+                  </ul>
+
+                  <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+                  <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+              </div>
+
+              <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+            </div>
+          </div>
+        </div>
+    </section>
+
+    <section class="contact">
+      <div class="container">
+        <div class="section-heading text-center">
+          <h2 class="text-white">Контактілер</h2>
+          <p class="text-white">Сұрақтарыңыз болса төмендегі форманы толтырып жіберіңіз</p>
+        </div>
+        <div class="row">
+          <div class="col-md-8 mx-auto">
+            <form class="uk-grid-small" uk-grid>
+                <div class="uk-width-1-2@s">
+                    <input class="uk-input uk-form-success" type="text" placeholder="Аты-жөні">
+                </div>
+                <div class="uk-width-1-2@s">
+                    <input class="uk-input uk-form-success" type="text" placeholder="Телефон">
+                </div>
+                <div class="uk-width-1-1@s">
+                    <textarea class="uk-textarea uk-form-success" rows="5" placeholder="Хабарлама"></textarea>
+                </div>
+                <div class="uk-width-1-1@s mx-auto">
+                  <a href="#" class="btn btn-outline btn-xl mx-auto uk-width-1-1@s">Жіберу</a>
+                </div>
+                
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="overlay"></div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <p>&copy; shpor.kz 2018.</p>
+        <ul class="list-inline">
+          <li class="list-inline-item">
+            <a href="https://api.whatsapp.com/send?phone=87777777777"><span uk-icon="whatsapp"></span></a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#"><span uk-icon="vk"></span></a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#"><span uk-icon="instagram"></span></a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+
+    <div id="modal-close-outside" uk-modal>
+      <div class="uk-modal-dialog uk-modal-body mt-5">
+          <button class="uk-modal-close-outside" type="button" uk-close></button>
+          <h2 class="uk-modal-title">Тапсырыс беру</h2>
+          <form class="uk-grid-small" uk-grid>
+              <div class="uk-width-1-1@s">
+                  <input class="uk-input uk-form-success" type="text" placeholder="Аты-жөні">
+              </div>
+              <div class="uk-width-1-1@s">
+                  <input class="uk-input uk-form-success" type="text" placeholder="Телефон">
+              </div>
+              <div class="uk-width-1-1@s mx-auto">
+                <a href="#" class="btn btn-outline btn-xl mx-auto uk-width-1-1@s">Жіберу</a>
+              </div>
+              
+          </form>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/new-age.js"></script>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+  </body>
+
+</html>
